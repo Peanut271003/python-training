6290d924ba3fa72655fefab4f55a8ca9881cd989
Peanut271003 <daongochai012@gmail.com>
2024-05-04 16:07:34 +0700

    init commit

--- day1/main.py
+++ day1/main.py
@@ -3,5 +3,5 @@ print("hello world") # Duplicate dùng Shift + Alt + Down
 print("My name is Hai") # di chuyen dong Alt + UP, Alt + DOWN 
 print("❤😋") # Search lenh Ctrl + Shift + P
 # Win + .
-print("❤😋") # Ctl + J open terminal
+print("❤😋") # Ctl + J open terminal, cau lenh upload len github: init commit
 
