15a9a459e7af6594f7c762266d041c49e8f49c86
Peanut271003 <daongochai012@gmail.com>
2024-05-12 11:17:48 +0700

    init commit

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,7 @@
-# CTRL "+" ,
+# CTRL "+" ,    để mở Settings
 print("hello world") # Duplicate dùng Shift + Alt + Down
 print("My name is Hai") # di chuyen dong Alt + UP, Alt + DOWN 
 print("❤😋") # Search lenh Ctrl + Shift + P
-# Win + .
+# Win + .  Để mở emoji
 print("❤😋") # Ctl + J open terminal, cau lenh upload len github: init commit
 
